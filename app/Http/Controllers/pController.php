8f30b7d4f6bc8fb8dccb0fceaf3e8dd737afcce7
akai <jack8485869@gmail.com>
2017-05-09 00:59:25 +0800

    first commit

---
+++ app/Http/Controllers/pController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class pController extends Controller {
+	public function index()
+    {
+    	return view('p');
+    }
+}
---
+++ resources/views/p.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>laravel</title>
+</head>
+<body>
+<div class="content">
+                <div class="title m-b-md">
+                    practice
+                </div>
+</body>
+</html>
